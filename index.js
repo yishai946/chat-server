8413211bf4173003e2353f2c8bd3d8033fbd764a
Yishai Biton <yishai946@gmail.com>
2024-04-01 17:32:06 +0300

    changed port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const socket = require("socket.io");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 
